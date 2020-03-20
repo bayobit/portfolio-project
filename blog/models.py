ee53618765fd308e2ec4e2cb4d0397ceafee9927
Ismail Tijani <ismaeelbit@gmail.com>
2020-03-20 10:41:22 +0000

    bootstrap done for job app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title =  models.CharField(max_length = 200)
+    pubdate = models.DateTimeField(null = True, blank = True)
+    body = models.TextField(default = 'DEFAULT VALUE', blank = True, null=True)
+    image = models.ImageField(upload_to = 'images/')
+    
